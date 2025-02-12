105a125f1c55d409fd308fb60c3cbf9cc8c97604
rafPH1998 <rafaelb89.rb@gmail.com>
2025-02-12 14:56:23 -0300

    feat: - criando a classe notification para implementacao do canal broadcast

---
+++ app/Notifications/MessageTestNotification.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Notifications\Messages\BroadcastMessage;
+use Illuminate\Notifications\Notification;
+
+class MessageTestNotification extends Notification implements ShouldBroadcast
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     */
+    public function __construct(private array $payload)
+    {
+        //
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via(object $notifiable): array
+    {
+        return ['broadcast'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     */
+  /*   public function toMail(object $notifiable): MailMessage
+    {
+        return (new MailMessage)
+                    ->line('The introduction to the notification.')
+                    ->action('Notification Action', url('/'))
+                    ->line('Thank you for using our application!');
+    } */
+
+    public function toBroadcast(object $notifiable): BroadcastMessage
+    {
+        return new BroadcastMessage([
+            'status' => $this->payload['status'],
+            'body' => $this->payload['body']
+        ]);
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(object $notifiable): array
+    {
+        return [
+            //
+        ];
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('notification', function () {
+    return view('welcome');
+});
+
+
+Route::get('login/{$id}', function (int $id) {
+    auth()->loginUsingId($id);
+});
